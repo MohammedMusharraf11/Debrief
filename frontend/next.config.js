d18dafac60e2d29e04a97488fcd5c0b945698f98
Mohammed Musharraf <86351406+MohammedMusharraf11@users.noreply.github.com>
2026-06-21 22:12:48 +0530

    Add visit route redirect

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/visit",
+        destination: "/log",
+        permanent: true,
+      },
+      {
+        source: "/home",
+        destination: "/log",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
